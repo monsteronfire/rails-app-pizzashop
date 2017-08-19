3fdcafe0c681fe63bac993793c4f329d63d77906
Shaii Ong <monsteronfire@gmail.com>
2017-08-19 20:54:29 +0800

    Add product resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817142158) do
+ActiveRecord::Schema.define(version: 20170819125047) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "category"
+    t.string "size"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
