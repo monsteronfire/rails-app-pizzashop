8f2ce937380e74337e4500662e3ccf031ece4f88
Shaii Ong <monsteronfire@gmail.com>
2017-08-26 23:02:03 +0800

    Add actions to product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
